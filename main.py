461306896ed4dd1415ba0da8dbcf663145c4e446
Nicholas Debeurre <debeurren@gmail.com>
2025-11-01 17:03:39 -0700

    remove echo tool, move identity to function

--- main.py
+++ main.py
@@ -3,30 +3,26 @@ import asyncio
 from src.agent import Agent
 from src.tools import tools, tool_descriptions
 
-def main():
-    # built the agent identity with the system prompt and instructions
-    #    - system prompt: tell the agent who it is and what it can do
-    #   - instructions: explain how to use the tools
+def agent_identity() -> str:
+    """Build the agent identity with system prompt and instructions."""
     system_prompt = "You are a tool-calling agent that may use tools by responding according to their instructions.\n"
     instructions = (
         "You may use the following tools to assist with user queries.\n"
         "Avoid using tools if the user query can be answered without them.\n"
         "Here are the tools you can use:\n"
         f"{tool_descriptions}\n"
-        "When you decide to use a tool, respond with the format:\n"
-        "'ToolName(arg)' where ToolName is the name of the tool and arg is the argument to pass to the tool.\n"
+        "When you decide to use a tool, respond with the format:"
+        "'ToolName(arg)' where ToolName is the name of the tool and arg is the argument to pass to the tool."
         "If the tool does not require an argument, use 'ToolName()'.\n"
         "Only use one tool per response.\n"
     )
-    agent_identity = system_prompt + instructions
+    return system_prompt + instructions
 
-    # build the agent for the session
+def main():
     agent = Agent(
         tools=tools,
-        identity=agent_identity
+        identity=agent_identity()
     )
-
-    # run the agent interaction loop
     agent.run()
 
 if __name__ == "__main__":
--- src/tools.py
+++ src/tools.py
@@ -10,22 +10,13 @@ class Tool:
     def run(self, arg: str) -> str:
         return self.func(arg) if arg != "" else self.func()
 
-# Example prompt to use the echo tool: "Please echo ‘hello world’"
-def echo_tool(text: str) -> str:
-    return f"Echo: {text}"
-
-# Example prompt to us the time tool: "What time is it?"
+# Example prompt to use the time tool: "What time is it?"
 def time_tool() -> str:
     now = datetime.now()
     return f"The current time is {now.strftime('%I:%M%p').lstrip('0').lower()} on {now.strftime('%d %B %Y')}"
 
 # Define available tools
 tools = [
-    Tool(
-        "Echo",
-        echo_tool,
-        "Prints the input text prefixed with Echo. Usage: return 'Echo(<text>)' where <text> is the text to echo."
-    ),
     Tool(
         "Time",
         time_tool,
